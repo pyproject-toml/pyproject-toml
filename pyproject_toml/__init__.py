441915c7b44e5e1007ac3d1bf9348a5d22992cc1
Tang Ziya <tcztzy@gmail.com>
2021-03-08 00:32:44 +0800

    ADD: setup

--- pyproject_toml/__init__.py
+++ pyproject_toml/__init__.py
@@ -1,5 +1,10 @@
-from . import build_system, project, tool
+from pathlib import Path
 
+import setuptools
+import toml
+from jsonschema import validate
+
+from . import build_system, project, tool
 
 SCHEMA = {
     "type": "object",
@@ -10,3 +15,54 @@ SCHEMA = {
         "tool": tool.__schema__,
     },
 }
+
+README_CONTENT_TYPES = {
+    ".md": "text/markdown",
+    ".rst": "text/x-rst",
+    ".txt": "text/plain",
+    "": "text/plain",
+}
+
+
+def setup(**attrs):
+    if (pyproject_toml := Path("pyproject.toml")).exists():
+        pyproject = toml.loads(pyproject_toml.read_text(encoding="utf-8"))
+        validate(pyproject, SCHEMA)
+        for k, v in pyproject.get("project", {}).items():
+            attrs[k] = v
+        for k, v in pyproject.get("tool", {}).get("pyproject-toml", {}).items():
+            attrs[k] = v
+
+    if "readme" in attrs:
+        readme = attrs.pop("readme")
+        if isinstance(readme, str):
+            readme = Path(readme)
+        elif "file" in readme:
+            readme = Path(readme["file"])
+        else:
+            attrs["long_description"] = readme["text"]
+        if isinstance(readme, Path):
+            try:
+                attrs["long_description_content_type"] = README_CONTENT_TYPES[readme.suffix]
+            except KeyError:
+                raise TypeError(f"Content type of {readme} is not supported")
+            attrs["long_description"] = readme.read_text(encoding="utf-8")
+
+    if "requires-python" in attrs:
+        attrs["python_requires"] = attrs.pop("requires-python")
+
+    if "license" in attrs:
+        license = attrs.pop("license")
+        if isinstance(license, str):
+            raise NotImplementedError(
+                "No PEP supports SPDX at 2021-03-07 when I write, see "
+                "https://www.python.org/dev/peps/pep-0621/#id88"
+            )
+        if "text" in license:
+            attrs["license"] = license["text"]
+        elif "file" in license:
+            attrs["license_file"] = license["file"]
+
+    attrs["install_requires"] = attrs.pop("dependencies", None)
+    
+    setuptools.setup(**attrs)
--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
-import setuptools
-import toml
+from pyproject_toml import setup
 
-with open("pyproject.toml") as pyproject_toml:
-    pyproject_toml = toml.load(pyproject_toml)
-    project = pyproject_toml["project"]
-    tool = pyproject_toml["tool"]["pyproject-toml"]
-with open(project.pop("readme"), encoding="utf-8") as readme:
-    project["long_description"] = readme.read()
-    project["long_description_content_type"] = "text/x-rst"
-license = project.pop("license")
-project["license"] = license["text"]
-project["install_requires"] = project.pop("dependencies", None)
-setuptools.setup(**tool, **project)
+setup()

780b602cb79317efa36e0826ddbe3fcec71a4104
Tang Ziya <tcztzy@gmail.com>
2021-04-29 23:41:45 +0800

    REFACTOR: use decorator to implement setup patch

--- pyproject_toml/__init__.py
+++ pyproject_toml/__init__.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from pathlib import Path
 from typing import Literal
+from functools import wraps
 
 import setuptools
 import toml
@@ -40,58 +41,65 @@ def format_author(author: dict[Literal["name", "email"], str]):
         raise ValueError("")
 
 
-def setup(**attrs):
-    if (pyproject_toml := Path("pyproject.toml")).exists():
-        pyproject = toml.loads(pyproject_toml.read_text(encoding="utf-8"))
-        validate(pyproject, SCHEMA)
-        for k, v in pyproject.get("project", {}).items():
-            attrs[k] = v
-        for k, v in pyproject.get("tool", {}).get("pyproject-toml", {}).items():
-            attrs[k] = v
+def setup_decorator(origin_setup):
+    @wraps(origin_setup)
+    def new_setup(**attrs):
+        if (pyproject_toml := Path("pyproject.toml")).exists():
+            pyproject = toml.loads(pyproject_toml.read_text(encoding="utf-8"))
+            validate(pyproject, SCHEMA)
+            for k, v in pyproject.get("project", {}).items():
+                attrs[k] = v
+            for k, v in pyproject.get("tool", {}).get("pyproject-toml", {}).items():
+                attrs[k] = v
 
-    if "readme" in attrs:
-        readme = attrs.pop("readme")
-        if isinstance(readme, str):
-            readme = Path(readme)
-        elif "file" in readme:
-            readme = Path(readme["file"])
-        else:
-            attrs["long_description"] = readme["text"]
-        if isinstance(readme, Path):
-            try:
-                attrs["long_description_content_type"] = README_CONTENT_TYPES[
-                    readme.suffix
-                ]
-            except KeyError:
-                raise TypeError(f"Content type of {readme} is not supported")
-            attrs["long_description"] = readme.read_text(encoding="utf-8")
+        if "readme" in attrs:
+            readme = attrs.pop("readme")
+            if isinstance(readme, str):
+                readme = Path(readme)
+            elif "file" in readme:
+                readme = Path(readme["file"])
+            else:
+                attrs["long_description"] = readme["text"]
+            if isinstance(readme, Path):
+                try:
+                    attrs["long_description_content_type"] = README_CONTENT_TYPES[
+                        readme.suffix
+                    ]
+                except KeyError:
+                    raise TypeError(f"Content type of {readme} is not supported")
+                attrs["long_description"] = readme.read_text(encoding="utf-8")
 
-    if "requires-python" in attrs:
-        attrs["python_requires"] = attrs.pop("requires-python")
+        if "requires-python" in attrs:
+            attrs["python_requires"] = attrs.pop("requires-python")
 
-    if "license" in attrs:
-        license = attrs.pop("license")
-        if isinstance(license, str):
-            raise NotImplementedError(
-                "No PEP supports SPDX at 2021-03-07 when I write, see "
-                "https://www.python.org/dev/peps/pep-0621/#id88"
-            )
-        if "text" in license:
-            attrs["license"] = license["text"]
-        elif "file" in license:
-            attrs["license_file"] = license["file"]
+        if "license" in attrs:
+            license = attrs.pop("license")
+            if isinstance(license, str):
+                raise NotImplementedError(
+                    "No PEP supports SPDX at 2021-03-07 when I write, see "
+                    "https://www.python.org/dev/peps/pep-0621/#id88"
+                )
+            if "text" in license:
+                attrs["license"] = license["text"]
+            elif "file" in license:
+                attrs["license_file"] = license["file"]
 
-    authors = attrs.pop("authors", [])
-    maintainers = attrs.pop("maintainers", [])
-    author_dict = defaultdict(list)
-    for type_, people in ("author", authors), ("maintainer", maintainers):
-        for person in people:
-            value, postfix = format_author(person)
-            author_dict[type_ + postfix].append(value)
+        authors = attrs.pop("authors", [])
+        maintainers = attrs.pop("maintainers", [])
+        author_dict = defaultdict(list)
+        for type_, people in ("author", authors), ("maintainer", maintainers):
+            for person in people:
+                value, postfix = format_author(person)
+                author_dict[type_ + postfix].append(value)
 
-    for k, v in author_dict.items():
-        attrs[k] = ",".join(v)
+        for k, v in author_dict.items():
+            attrs[k] = ",".join(v)
 
-    attrs["install_requires"] = attrs.pop("dependencies", None)
+        attrs["install_requires"] = attrs.pop("dependencies", None)
 
-    setuptools.setup(**attrs)
+        origin_setup(**attrs)
+
+    return new_setup
+
+
+setup = setup_decorator(setuptools.setup)
